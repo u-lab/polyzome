f628b519d1dd95be24c2d27bd476123f6d1e827c
SeigoMori <85787242+SeigoMori@users.noreply.github.com>
2021-09-23 00:11:25 +0900

    KP進捗2021/09/23

--- rain/seigo_main.ino
+++ rain/seigo_main.ino
@@ -230,8 +230,19 @@ void allLighting (){
   drawPoints(plane);
 }
 
+/*
+** 関数名 : fadeIn
+** 引数 : int fade : フェードインのスピード(基準:20)
+** 戻り値 : なし
+** 関数の機能 : <説明>
+** 作者: 関数作った人
+** 日付: 関数作った日
+*/
+
+
 //フェードインで全点灯
-void fadeIn (){
+//time 20ぐらいが適正
+void fadeIn (int fade){
   int heights[5]={1,1,1,1,1};
   setLightHeights(heights);
   for(float i=0; i<=1; i+=0.01){
@@ -243,29 +254,50 @@ void fadeIn (){
       {i,i,i,i,i}
     };
     drawPoints(plane);
-    delay(10);
+    delay(fade);
   }
 }
 
-//死の表現1つ目
-void deathFirst(){
+/*
+** 関数名 : <name>
+** 引数 : <型> <変数名> : <役割>
+** 戻り値 : <型> <変数名> : <役割>
+** 関数の機能 : <説明>
+** 作者: 関数作った人
+** 日付: 関数作った日
+*/
+/*
+** 関数名 : deathFirst
+** 引数 : int time : ライトが変わるスピード(基準:500)
+** 戻り値 : なし
+** 関数の機能 : 死の表現1つ目、上段から下段に消えていき、上段から下段についていく。
+5*5*5の光っている箱と暗い箱が交互に下に押し出され、消えていくイメージ
+** 作者: seigo
+** 日付: 2021/09/22
+*/
+void deathFirst(int time){
   //上から一段づつ消す
   for(int i=4; i>=0; i--){
     setLightHeight(i, false);
-    delay(500);
+    delay(time);
   }
   //上から一段づつ点ける
   for(int i=4; i>0; i--){
     setLightHeight(i, true);
-    delay(500);
+    delay(time);
   }
 }
 
-//死の表現2つ目
-//柱がランダムに消えていく
-void deathSecond(){
+/*
+** 関数名 : deathSecond
+** 引数 : int time : 柱が消えるスピード(基準:100)
+** 戻り値 : なし
+** 関数の機能 : 死の表現2つ目、柱がランダムに消えていき、ランダムについていく
+** 作者: seigo
+** 日付: 2021/09/22
+*/
+void deathSecond(int time){
   int downNumbers[25]={25,16,11,15,28,30,20,14,23,22,26,24,9,12,21,27,7,18,13,8,10,19,17,29,6};
-  int time=200;
   for(int i=0; i<25; i++){
     dmx_master.setChannelValue(downNumbers[i], 0);
     delay(time);
@@ -278,45 +310,50 @@ void deathSecond(){
   }
 }
 
-//死の表現3つ目
-void deathThird(){
+/*
+** 関数名 : deathThird
+** 引数 : int time : 立方体が消えるスピード(基準:600)
+** 引数 : int fade : フェードインのスピード(基準:20)
+** 戻り値 : なし
+** 関数の機能 : 死の表現3つ目、外側から立方体が消えていき、フェードインで全点灯
+** 作者: seigo
+** 日付: 2021/09/22
+*/
+void deathThird(int time, int fade){
   //外側から立方体が消えていく
   //第一段階
-  int firstHeights[5]={0,1,1,1,0};
-  setLightHeights(firstHeights);
-  float firstPlane[][5]={
-    {0,0,0,0,0},
-    {0,1,1,1,0},
-    {0,1,1,1,0},
-    {0,1,1,1,0},
-    {0,0,0,0,0}
-  };
-  drawPoints(firstPlane);
-  delay(1000);
+  dmx_master.setChannelValue(1, 0);
+  dmx_master.setChannelValue(5, 0);
+  for(int i=6; i<=30; i++){
+    if(! ((i>=12 && i<=14) || (i>=17 && i<=19) || (i>=22 && i<=24))) dmx_master.setChannelValue(i, 0);
+  }
+  delay(time);
   //第二段階
-  int secondHeights[5]={0,0,1,0,0};
-  setLightHeights(secondHeights);
-  float secondPlane[][5]={
-    {0,0,0,0,0},
-    {0,0,0,0,0},
-    {0,0,1,0,0},
-    {0,0,0,0,0},
-    {0,0,0,0,0}
-  };
-  drawPoints(secondPlane);
-  delay(1000);
+  dmx_master.setChannelValue(2, 0);
+  dmx_master.setChannelValue(4, 0);
+  for(int i=12; i<=24; i++){
+    if(i!=18) dmx_master.setChannelValue(i, 0);
+  }
+  delay(time);
   //全消灯
   clearAll();
+  delay(time);
   //フェードインで全点灯
-  fadeIn();
+  fadeIn(fade);
 }
 
-//分解の表現3つ目
-//全てのライトがフェードアウト
-void diffusionThird(){
+/*
+** 関数名 : diffusionThird
+** 引数 : int fade : フェードアウト・インのスピード(基準:20)
+** 戻り値 : なし
+** 関数の機能 : 分解の表現3つ目、全てのライトがフェードアウトし、フェードインで全点灯
+** 作者: seigo
+** 日付: 2021/09/22
+*/
+void diffusionThird(int fade){
   int heights[5]={1,1,1,1,1};
   setLightHeights(heights);
-  for(float i=0; i<=1; i+=0.01){
+  for(float i=1; i>=0; i-=0.01){
     float plane[][5]={
       {i,i,i,i,i},
       {i,i,i,i,i},
@@ -325,17 +362,20 @@ void diffusionThird(){
       {i,i,i,i,i}
     };
     drawPoints(plane);
-    delay(10);
+    delay(fade);
   }
+  fadeIn(fade);
 }
 
 //メインループ
 void loop(){
   //動作確認
   //allLighting();
-  //deathFirst();
-  //deathSecond();
-  //deathThird();
+  delay(500);
+  //deathFirst(600);
+  //deathSecond(200);
+  //deathThird(600, 20);
+  //diffusionThird(20);
 
   //clearAll();
 }
